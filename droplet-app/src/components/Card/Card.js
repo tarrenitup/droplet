19f34212ce060dfcfeb55ad157a8b5404dd204d0
Tarren Engberg <tarren@outlook.com>
2019-01-25 21:10:57 -0800

    fetch test API data in home page posts.

--- droplet-app/src/components/Card/Card.js
+++ droplet-app/src/components/Card/Card.js
@@ -2,22 +2,45 @@ import React, { Component } from 'react'
 import './Card.css'
 import likesIcon from './likes.svg'
 
-const Card = (props) => (
-    <div className='card'>
-        <div className='card-top'>
-            <img className='profile-pic' src={props.profilePic.pic} alt={props.profilePic.alt} />
-            <p className='username'>{props.username}</p>
-        </div>
-        
-        <p className='card-text'>{props.text}</p>
-       
-        <div className='card-bottom'>
-            <div className='likes'>
-                <img src={likesIcon} alt='The likes icon'/>
-                <p>{props.likes}</p>
+
+class Card extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            postText: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://randomuser.me/api/?dataType=json")
+            .then(results => {
+                return results.json()
+            }).then(data => {
+                this.setState({postText: data.results[0].email})
+            })
+    }
+
+    render() {
+        return (
+            <div className='card'>
+                <div className='card-top'>
+                    <img className='profile-pic' src={this.props.profilePic.pic} alt={this.props.profilePic.alt} />
+                    <p className='username'>{this.props.username}</p>
+                </div>
+                
+                <p className='card-text'>{this.state.postText}</p>
+            
+                <div className='card-bottom'>
+                    <div className='likes'>
+                        <img src={likesIcon} alt='The likes icon'/>
+                        <p>{this.props.likes}</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default Card;
