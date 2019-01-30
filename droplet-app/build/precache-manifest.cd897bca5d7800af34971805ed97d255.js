8188f4a3fcfafb429942d8c9e6aa8138828c4495
Brice Ng <bricefive@gmail.com>
2019-01-29 16:42:57 -0800

    Updated Build and Doc

---
+++ droplet-app/build/precache-manifest.cd897bca5d7800af34971805ed97d255.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c1425399aa96f41eb6",
+    "url": "/static/css/main.b31b749e.chunk.css"
+  },
+  {
+    "revision": "35c1425399aa96f41eb6",
+    "url": "/static/js/main.35c14253.chunk.js"
+  },
+  {
+    "revision": "2f970137f1e41fa410d4",
+    "url": "/static/js/1.2f970137.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b2cdb33675b20cc25053cc8bdc9e312",
+    "url": "/static/media/profile.2b2cdb33.jpg"
+  },
+  {
+    "revision": "7bfce66b626533de44a43a7b2d3e2222",
+    "url": "/static/media/likes.7bfce66b.svg"
+  },
+  {
+    "revision": "ed1a8e78957e50b2e1685e91fae5dbcd",
+    "url": "/static/media/home.ed1a8e78.svg"
+  },
+  {
+    "revision": "17f9d2f714c2efb48c7d57424250df8a",
+    "url": "/static/media/map.17f9d2f7.svg"
+  },
+  {
+    "revision": "01d2fb21a9758c1485ea73f9f64630d5",
+    "url": "/static/media/new.01d2fb21.svg"
+  },
+  {
+    "revision": "9643f2c0a8fca3d21ce934e3bc81c7d2",
+    "url": "/static/media/notifications.9643f2c0.svg"
+  },
+  {
+    "revision": "2453618860b336a0bfe6074b5d9404dc",
+    "url": "/static/media/profile.24536188.svg"
+  },
+  {
+    "revision": "273354441c5e728aebd3ee512c17f2d5",
+    "url": "/index.html"
+  }
+];
