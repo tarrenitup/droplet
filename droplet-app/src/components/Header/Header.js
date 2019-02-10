b32a3ca2770ea05e4c5ea77f8a8609b14bbc23aa
Tarren Engberg <tarren@outlook.com>
2019-02-09 21:58:50 -0800

    Very basic start of Redux added to theme control in header.

--- droplet-app/src/components/Header/Header.js
+++ droplet-app/src/components/Header/Header.js
@@ -2,9 +2,41 @@ import React from 'react'
 import './Header.css'
 import Logo from './logo.png'
 
+import { createStore, combineReducers } from 'redux'
+
+const createThemeDotAction = (currentTheme) => ({
+    type: 'TOGGLE_THEME',
+    payload: {
+        currentTheme: currentTheme
+    }
+})
+
+const themeDotReducer = (state = false, action) => {
+    if (action.type === 'TOGGLE_THEME') {
+        return {
+            ...state,
+            currentTheme: !state.currentTheme
+        }
+    } else {
+        return state
+    }
+}
+
+const rootReducer = combineReducers({
+    themeDotReducer: themeDotReducer
+})
+
+const store = createStore(rootReducer)
+
+store.dispatch(createThemeDotAction(true))
+store.dispatch(createThemeDotAction(false))
+
+console.log(store.getState())
+
 const Header = () => (
     <header className="app-header">
         <img className='logo' src={Logo} alt='Droplet logo' />
+        <div className='theme-dot' alt='Theme switcher'></div>
     </header>
 )
 
