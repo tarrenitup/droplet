f7709f60d83a3d0b219574ab8db615a66658135b
James Luo <luoja@oregonstate.edu>
2019-03-13 15:41:52 -0700

    Fixed redirect stuff

--- droplet-app/src/components/TestScreen/Test.js
+++ droplet-app/src/components/TestScreen/Test.js
@@ -5,6 +5,10 @@ import Auth from '../Auth/Auth.js'
 class Test extends Component{
     constructor(){
         super();
+        this.state = {
+            messages: [],
+        }
+        this.onGetUserPosts = this.onGetUserPosts.bind(this);
     }
 
     onGetLocation(event){
@@ -21,9 +25,24 @@ class Test extends Component{
     onGetID(event){
         event.preventDefault();
         console.log(Auth.getCookie('token'));
+        //need to check if cookie is alive
         console.log(Auth.parseJwt(Auth.getCookie('token')).sub);
     }
 
+    onGetUserPosts(event){
+        event.preventDefault();
+        const fetchURL = 'http://localhost:5000/users/' + Auth.parseJwt(Auth.getCookie('token')).sub;
+        console.log(fetchURL);
+        fetch(fetchURL)
+            .then(results => {
+                return results.json()
+            }).then(data =>{
+                this.setState({
+                    messages: data.message,
+                })
+            })
+        console.log(this.state.messages);
+    }
     render(){
         return(
             <main className="test-screen">
@@ -39,6 +58,12 @@ class Test extends Component{
                     value="Get User ID"
                     type="submit"
                     onClick={this.onGetID}
+                />
+                <input
+                    className="getUserPosts"
+                    value="Get User Posts"
+                    type="submit"
+                    onClick={this.onGetUserPosts}
                 />
                 </form>
             </main>
