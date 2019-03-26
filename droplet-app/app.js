f02b07a333dc69aaae59250bd0ee3ba8eebf8ee9
Brice Ng <bricefive@gmail.com>
2019-03-25 20:18:27 -0700

    Updated app.js
    Fix error where post route was not defined

--- droplet-app/app.js
+++ droplet-app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -9,13 +10,12 @@ const bodyParser = require('body-parser');
 //require('./models/comment');
 
 //Routes
-const postRoutes = require('./api/routes/users');
+const userRoutes = require('./api/routes/users');
+const postRoutes = require('./api/routes/posts');
 
-mongoose.connect('mongodb://admin:'+ process.env.MONGO_ATLAS_PW +'@droplet-shard-00-00-8shns.mongodb.net:27017,droplet-shard-00-01-8shns.mongodb.net:27017,droplet-shard-00-02-8shns.mongodb.net:27017/test?ssl=true&replicaSet=Droplet-shard-0&authSource=admin&retryWrites=true',
-{
-    useNewUrlParser: true
+const mongoDBName = process.env.MONGO_DATABASE || 'test';
 
-});
+mongoose.connect('mongodb://admin:'+ process.env.MONGO_ATLAS_PW +'@droplet-shard-00-00-8shns.mongodb.net:27017,droplet-shard-00-01-8shns.mongodb.net:27017,droplet-shard-00-02-8shns.mongodb.net:27017/test?ssl=true&replicaSet=Droplet-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser: true});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -24,8 +24,7 @@ app.use(bodyParser.json());
 //CORS error handling
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    //Used to be '*'
-    res.header('Access-Control-Allow-Headers', 'Content-type, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
@@ -34,7 +33,8 @@ app.use((req, res, next) => {
 });
 
 //Routes to handle requests
-app.use('/users', postRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 //404 error handling
 app.use((req, res, next) => {
@@ -50,7 +50,7 @@ app.use((error, req, res, next) => {
         error: {
             message: error.message
         }
-     });
+    });
 });
 
 module.exports = app;
