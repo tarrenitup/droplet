ce0e31e7a906b9515051825eb0a89bb39f8a2719
Dennis Li <dennisli@Denniss-MacBook-Pro.local>
2019-04-12 16:50:18 -0700

    load posts based off mapscreen working

--- droplet-app/src/components/MapScreen/MapScreen.js
+++ droplet-app/src/components/MapScreen/MapScreen.js
@@ -5,23 +5,83 @@ import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './MapScreen.css'
 import Logo from './logo.png'
-import { loadHomePosts } from '../../actions/postActions'
+import { loadMapPosts } from '../../actions/postActions'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGlkZW5uIiwiYSI6ImNqcmg2NDU5czA4b3A0M25udmUxcWpjcmEifQ.J9ThJ9sMDK7ANhYkSpVnyg';
 
 class Map extends React.Component {
 
   constructor(props) {
-    props.dispatch(loadHomePosts())
-
     super(props);
-    console.log(props.homePosts)
-
+    console.log(props.mapPosts)
     this.state = {
       lng: -122,
       lat: 45,
       zoom: 5
     };
+  }
+
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+    this.updatePosts(map)
+    console.log("mounted")
+
+
+
+    // Add geolocate control to the map.
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }), 'top-left');
+
+    map.on('touchend', () => {
+      this.updatePosts(map)
+    })
+    map.on('zoom', ()=>{
+      this.updatePosts(map)
+    })
+    map.on('move', () => {
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+
+  }
+
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+    return (
+        <main ref={el => this.mapContainer = el} className="map-screen" ></main>
+    );
+  }
+
+  updatePosts(map){
+    const { lng, lat } = map.getCenter();
+    const bounds = map.getBounds();
+    const dist = distance(lat,lng,bounds.getNorthWest().lat,bounds.getNorthWest().lng, "K");
+    const meterRadius = dist *1000
+    this.props.dispatch(loadMapPosts(lng, lat, meterRadius))
+    console.log(this.props.mapPosts)
+    for(var i = 0; i < this.props.mapPosts.length; i++){
+      const longitude = this.props.mapPosts[i].location.coordinates[0]
+      const latitude = this.props.mapPosts[i].location.coordinates[1]
+      const username = this.props.mapPosts[i].username
+      const data = this.props.mapPosts[i].content
+      this.createMarker(longitude, latitude, map, username, data);
+    }
   }
 
   createMarker(lng, lat, map, popupName, popupData, popupImage){
@@ -41,55 +101,10 @@ class Map extends React.Component {
       .addTo(map)
       .setPopup(popup);
   }
-
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
-    });
-
-    // Add geolocate control to the map.
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true
-    }), 'top-left');
-
-
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
-      const bounds = map.getBounds();
-      const dist = distance(lat,lng,bounds.getNorthWest().lat,bounds.getNorthWest().lng, "K");
-      const meterRadius = dist *1000
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
-
-    for(var i = 0; i < this.props.homePosts.length; i++){
-      const longitude = this.props.homePosts[i].location.coordinates[0]
-      const latitude = this.props.homePosts[i].location.coordinates[1]
-      const username = this.props.homePosts[i].username
-      const data = this.props.homePosts[i].content
-      this.createMarker(longitude, latitude, map, username, data);
-    }
-
-  }
-
-  render() {
-    const { lng, lat, zoom } = this.state;
-    return (
-        <main ref={el => this.mapContainer = el} className="map-screen" ></main>
-    );
-  }
 }
+
+
+
 
 //GeoDataSource.com
 function distance(lat1, lon1, lat2, lon2, unit) {
@@ -116,7 +131,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 
 function mapStateToProps(state) {
     return {
-      homePosts: state.homePosts
+      mapPosts: state.mapPosts
     }
 }
 
