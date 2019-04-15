3d96c42421359ea99bbdb65a7f022003097d68be
Tarren Engberg <tarren@outlook.com>
2019-04-15 14:40:11 -0700

    Push for Luo.

--- droplet-app/src/components/Overlay/Overlay.js
+++ droplet-app/src/components/Overlay/Overlay.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './Overlay.css'
 
-const overlayStyleClass = (visibility) => visibility ? 'overlay' : 'overlay off'
+const overlayStyleClass = visibility => visibility ? 'overlay' : 'overlay off'
+const spinnerStyleClass = spinner => spinner ? 'spinner' : 'spinner off'
 
 const Overlay = (props) => (
-    <div className={overlayStyleClass(props.visible)}></div>
+    <div className={overlayStyleClass(props.visible)}>
+        <div className={spinnerStyleClass(props.spinner)}>
+            <div className="double-bounce1"></div>
+            <div className="double-bounce2"></div>
+        </div>
+    </div>
 )
 
 const mapStateToProps = (state) => {
-    return { visible: state.overlay }
+    return { 
+        visible: state.overlay,
+        spinner: state.loadingSpinner
+    }
 }
 
 export default connect(mapStateToProps)(Overlay);
--- droplet-app/src/reducers/addPostReducer.js
+++ droplet-app/src/reducers/addPostReducer.js
@@ -1,9 +1,9 @@
 import * as types from '../actions/actionTypes';  
 import initialState from './initialState';
 
-export default function addPostReducer(state = initialState, action) {  
+export default function newPostAddSuccess(state = initialState, action) {  
   switch(action.type) {
-    case types.ADD_POSTS_SUCCESS:
+    case types.NEW_POST_ADD_SUCCESS:
       return action.posts
     default: 
       return state;
