86892aef953e74ecedc6311dfac80389bfebc0c9
Tarren Engberg <tarren@outlook.com>
2019-04-27 03:58:27 -0700

    added sass. Start making marker for current page.

--- droplet-app/src/components/Footer/Footer.js
+++ droplet-app/src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import './Footer.scss'
+import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { toggleNewPostModal } from '../../actions/postActions'
 
@@ -15,11 +15,11 @@ const Footer = (props) => {
         <footer className='app-footer'>
             <nav>
                 <ul className='nav-buttons'>
-                    <li><Link to={'/'}><img src={homeIcon} alt='Home screen icon'/></Link></li>
-                    <li><Link to={'/map'}><img src={mapIcon} alt='Map screen icon'/></Link></li>
+                    <li><NavLink activeClassName='selected' to={'/'}><img src={homeIcon} alt='Home screen icon'/></NavLink></li>
+                    <li><NavLink activeClassName='selected' to={'/map'}><img src={mapIcon} alt='Map screen icon'/></NavLink></li>
                     <li><img onClick={() => {props.dispatch(toggleNewPostModal())}} src={newIcon} alt='New Droplet screen icon'/></li>
-                    <li><Link to={'/likes'}><img src={notificationsIcon} alt='Notification screen icon'/></Link></li>
-                    <li><Link to={'/profile'}><img src={profileIcon} alt='Profile screen icon'/></Link></li>
+                    <li><NavLink activeClassName='selected' to={'/likes'}><img src={notificationsIcon} alt='Notification screen icon'/></NavLink></li>
+                    <li><NavLink activeClassName='selected' to={'/profile'}><img src={profileIcon} alt='Profile screen icon'/></NavLink></li>
                 </ul>
             </nav>
         </footer>
