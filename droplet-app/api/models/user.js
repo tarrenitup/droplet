a80dee2fa181a140467708a454f3ed28c49a33d8
Brice Ng <bricefive@gmail.com>
2019-05-13 10:33:30 -0700

    Updated for Demo

--- droplet-app/api/models/user.js
+++ droplet-app/api/models/user.js
@@ -15,7 +15,11 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Post',
         required: 'Post is required'
-    }]
+    }],
+    bio: {
+        type: String,
+        required: true
+    }
 });
 
 userSchema.pre('remove', function(next) {
@@ -27,5 +31,21 @@ userSchema.post('remove', function(next){
   console.log({ posts: post._id });
   userSchema.remove({ posts: post._id });
 });
+
+userSchema.update = function(req, res, next) {
+    const password = req.body.password;
+    bcrypt.hash(password, 10, function (err, hash) {
+        if(err){
+            return next(err);
+        }
+        req.body.password = hash;
+        User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, user) {
+            if(err){
+                return res.send(err).status(500);
+            }
+            return res.send(user).status(200);
+        });
+    });
+}
 
 module.exports = mongoose.model('User', userSchema);
