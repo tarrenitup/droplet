40a1bde67f5e56c1ebe8d4819226678289e4866c
James Luo <luoja@oregonstate.edu>
2019-05-16 16:34:10 -0700

    Fixed new post issue (a commented send statement)

--- droplet-app/api/routes/posts.js
+++ droplet-app/api/routes/posts.js
@@ -100,8 +100,9 @@ router.post('/:userId', upload.single('postImage'), async (req, res, next) => {
     //Get photo to upload
     //console.log(req.file);
     //Find any one user to post on
-    const user = await User.findOne({_id: req.params.userId});
-
+    const user = await User.findOne({_id: req.params.userId}).catch(error=>{
+        return res.send(error);
+    });
     //If file found, upload post accordingly
     if(req.file != undefined) {
         //Create new post with image
@@ -119,7 +120,6 @@ router.post('/:userId', upload.single('postImage'), async (req, res, next) => {
 
         //Associates the comment with a Post
         user.posts.push(post._id);
-
         //Save the post (so post is now in posts array)
         await user.save().catch(error=>{
             return res.status(500).send(error);
@@ -136,7 +136,6 @@ router.post('/:userId', upload.single('postImage'), async (req, res, next) => {
         post.content = req.body.content;
         post.postImage = undefined;
         post.location = req.body.location;
-
         //Save it
         await post.save().catch(error=>{
             return res.send(error);
@@ -147,7 +146,7 @@ router.post('/:userId', upload.single('postImage'), async (req, res, next) => {
         await user.save().catch(error=>{
             return res.send(error);
         });
-        //res.send(post);
+        res.send(post);
     }
 });
 
